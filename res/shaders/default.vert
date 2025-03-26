c617efb657fd34069fed8d0736cd18a535914184
Cann-t <xvrabl06@stud.fit.vut.cz>
2025-03-26 17:20:49 +0100

    THEY SPINNIN

--- res/shaders/default.vert
+++ res/shaders/default.vert
@@ -16,11 +16,14 @@ out vec2 texCoord;
 // Imports the camera matrix from the main function
 uniform mat4 camMatrix;
 
+// model uniform i hope
+uniform mat4 modelMatrix;
+
 
 void main()
 {
 	// Outputs the positions/coordinates of all vertices
-	gl_Position = camMatrix * vec4(aPos, 1.0);
+	gl_Position = camMatrix * modelMatrix * vec4(aPos, 1.0);
 	// Assigns the colors from the Vertex Data to "color"
 	color = aColor;
 	// Assigns the texture coordinates from the Vertex Data to "texCoord"
--- src/main.cpp
+++ src/main.cpp
@@ -168,6 +168,19 @@ int main()
 	double currTime, prevTime, timeDiff = 0.0f;
 	unsigned int counter = 0;
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,  0.0f,  0.0f), 
+		glm::vec3( 2.0f,  5.0f, -15.0f), 
+		glm::vec3(-1.5f, -2.2f, -2.5f),  
+		glm::vec3(-3.8f, -2.0f, -12.3f),  
+		glm::vec3( 2.4f, -0.4f, -3.5f),  
+		glm::vec3(-1.7f,  3.0f, -7.5f),  
+		glm::vec3( 1.3f, -2.0f, -2.5f),  
+		glm::vec3( 1.5f,  2.0f, -2.5f), 
+		glm::vec3( 1.5f,  0.2f, -1.5f), 
+		glm::vec3(-1.3f,  1.0f, -1.5f)  
+	};
+
 
 	Camera camera(window_width, window_height, glm::vec3(0.0f, 0.0f, 3.0f));
 
@@ -201,8 +214,23 @@ int main()
 		wall.Bind();
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, sizeof(vertices)/sizeof(int), GL_UNSIGNED_INT, 0);
+
+		for(int i = 0; i <= 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i; 
+			if(i % 3 == 0)
+			{
+				angle = glfwGetTime() * 25.0f;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			}
+
+			GLuint model_unifrom =  glGetUniformLocation(shaderProgram.ID, "modelMatrix");
+			glUniformMatrix4fv(model_unifrom, 1, GL_FALSE, glm::value_ptr(model));
+			// Draw primitives, number of indices, datatype of indices, index of indices
+			glDrawElements(GL_TRIANGLES, sizeof(vertices)/sizeof(int), GL_UNSIGNED_INT, 0);
+		}
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
